c4d950c38085e15c3a9e145931f4146e2d9fd893
tuqiang01 <tuqiang01@baidu.com>
2018-01-05 11:58:14 +0800

    添加单元测试，修改语法

---
+++ test/type.test.js
@@ -0,0 +1,29 @@
+/**
+ * @file 类型函数测试
+ * @author tuqiang_ru@163.com）
+ */
+
+import _ from '../src/index';
+import {expect} from 'chai';
+
+describe('Type judgment', () => {
+    it('string', () => {
+        expect(_.isString('string')).to.be.ok;
+    });
+    it('array', () => {
+        expect(_.isArray([])).to.be.ok;
+    });
+    it('object', () => {
+        expect(_.isObject({})).to.be.ok;
+    });
+    it('function', () => {
+        expect(_.isFunction(() => null)).to.be.ok;
+    });
+    it('Float32Array', () => {
+        expect(_.isFloat32Array([])).to.not.be.ok;
+        expect(_.isFloat32Array(new Float32Array())).to.be.ok;
+    });
+    it('null string', () => {
+        expect(_.isNullString('')).to.be.ok;
+    });
+});
